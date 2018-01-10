edbdbfbf8adc69827ee49ebb3b77f0ed95f24431
vinayprabhu19 <vinayprabhu19@gmail.com>
2018-01-10 23:12:44 +0530

    modification

--- Distance.py
+++ Distance.py
@@ -3,4 +3,4 @@ import math
 x1,y1=map(int,input("Enter first city coordinaes\n").split())
 x2,y2=map(int,input("Enter second city coordinaes\n").split())
 dist=math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
-print("Distance is %.2f"%dist)
+print("Distance is %.2f : "%dist)
